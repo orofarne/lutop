495683854522e93b6a95a159ec7b5af01a50c34b
Maxim Dementyev <max@orofarne.ru>
2013-09-26 00:24:11 +0400

    decode

--- src/Encode.cpp
+++ src/Encode.cpp
@@ -1,6 +1,9 @@
 #include "Encode.hpp"
 
 #include <msgpack.h>
+#include "msglen.h"
+
+#include <string.h>
 
 #include <stdexcept>
 
@@ -78,8 +81,94 @@ std::shared_ptr<Response> decodeResponse(std::string const &buf) {
     msgpack_unpacked msg;
     msgpack_unpacked_init(&msg);
     ASSERTPP( msgpack_unpack_next(&msg, buf.data(), buf.size(), nullptr) );
+    msgpack_object obj = msg.data;
+    ASSERTPP(obj.type == MSGPACK_OBJECT_MAP);
 
     std::shared_ptr<Response> r{new Response};
+
+    for(int i = 0; i < obj.via.map.size; ++i) {
+        msgpack_object key_obj = obj.via.map.ptr[i].key;
+        msgpack_object val = obj.via.map.ptr[i].val;
+
+        ASSERTPP(key_obj.type == MSGPACK_OBJECT_RAW);
+        msgpack_object_raw key = key_obj.via.raw;
+
+        if(key.size == 5 && 0 == strncmp("state", key.ptr, 5)) {
+            if(val.type == MSGPACK_OBJECT_NIL) {
+                // do nosing
+            }
+            else {
+                const char *ptr = key.ptr + key.size;
+                char *err = nullptr;
+                size_t max_len = buf.size() -
+                    (reinterpret_cast<const char *>(ptr) - reinterpret_cast<const char *>(buf.data()));
+                void *void_ptr = const_cast<void *>(reinterpret_cast<const void *>(ptr));
+                size_t m_len = ::msgpackclen_buf_read(void_ptr, max_len, &err);
+                if(err)
+                    throw std::runtime_error(std::string("msgpackclen_buf_read error: ") + err);
+
+                r->state.assign(ptr, m_len);
+            }
+        }
+        else if(key.size == 5 && 0 == strncmp("value", key.ptr, 5)) {
+            switch(val.type) {
+                case MSGPACK_OBJECT_NIL:
+                    r->value = Value();
+                    break;
+                case MSGPACK_OBJECT_BOOLEAN:
+                    r->value = Value(Value::Type::Boolean, (bool)val.via.boolean);
+                    break;
+                case MSGPACK_OBJECT_POSITIVE_INTEGER:
+                    r->value = Value(Value::Type::Numeric, val.via.u64);
+                    break;
+                case MSGPACK_OBJECT_NEGATIVE_INTEGER:
+                    r->value = Value(Value::Type::Numeric, val.via.i64);
+                    break;
+                case MSGPACK_OBJECT_DOUBLE:
+                    r->value = Value(Value::Type::Numeric, val.via.dec);
+                    break;
+                case MSGPACK_OBJECT_RAW:
+                    r->value = Value(Value::Type::String, std::string(val.via.raw.ptr, val.via.raw.size));
+                    break;
+                case MSGPACK_OBJECT_ARRAY:
+                case MSGPACK_OBJECT_MAP:
+                    {
+                        const char *ptr = key.ptr + key.size;
+                        char *err = nullptr;
+                        size_t max_len = buf.size() -
+                            (reinterpret_cast<const char *>(ptr) - reinterpret_cast<const char *>(buf.data()));
+                        void *void_ptr = const_cast<void *>(reinterpret_cast<const void *>(ptr));
+                        size_t m_len = ::msgpackclen_buf_read(void_ptr, max_len, &err);
+                        if(err)
+                            throw std::runtime_error(std::string("msgpackclen_buf_read error: ") + err);
+
+                        r->value = Value(Value::Type::Table, std::string(ptr, m_len));
+                    }
+                    break;
+
+            }
+        }
+        else if(key.size == 9 && 0 == strncmp("timestamp", key.ptr, 9)) {
+            switch(val.type) {
+                case MSGPACK_OBJECT_POSITIVE_INTEGER:
+                    r->timestamp = val.via.u64;
+                    break;
+                case MSGPACK_OBJECT_NEGATIVE_INTEGER:
+                    r->timestamp = val.via.i64;
+                    break;
+                case MSGPACK_OBJECT_DOUBLE:
+                    r->timestamp = val.via.dec;
+                    break;
+                default:
+                    throw std::runtime_error("Invalid type for timestamp");
+            }
+        }
+        else if(key.size == 5 && 0 == strncmp("error", key.ptr, 5)) {
+            ASSERTPP(val.type == MSGPACK_OBJECT_RAW);
+            r->error.assign(val.via.raw.ptr, val.via.raw.size);
+        }
+
+    }
 
     return r;
 }
